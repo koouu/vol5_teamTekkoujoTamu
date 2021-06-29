d994097bafc364391a6aa6afa4321bc9d3c9b90f
koouu <nkouta0420@gmail.com>
2021-06-30 01:38:39 +0900

    homecreated

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def privacy_policy
+  end
+
+  def terms_of_service
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'help', to: 'static_pages#help'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  get 'terms_of_service', to: 'static_pages#terms_of_service'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
