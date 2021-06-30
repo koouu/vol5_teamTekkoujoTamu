6b96eadb2d9665d315bd73876bc3d2a8d1a96b1d
koouu <nkouta0420@gmail.com>
2021-07-01 01:18:01 +0900

    created login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get 'help', to: 'static_pages#help'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
   get 'terms_of_service', to: 'static_pages#terms_of_service'
   get  '/signup',  to: 'users#new'
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
