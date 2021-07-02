d29b01743a2a2ce7858327dfd773706eda822d93
koouu <nkouta0420@gmail.com>
2021-07-03 03:54:49 +0900

    created login

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,17 +1,2 @@
-// Load all the channels within this directory and all subdirectories.
-// Channel files must be named *_channel.js.
-
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-$(function() {
-    var height=$("#header").height();
-    var height2=$("#home-cr").height();
-    $("body").css("margin-top", height);
-    $("#home").css("margin-top", height2+40);
-  });
-  //投稿画面分のマージンをとる
-$(function() {
-    var width=$("#child2").width();
-    $("#child1").css("margin-right", width);
-    $("footer").css("margin-right", width);
-  });
